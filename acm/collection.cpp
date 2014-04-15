e345a8d2765617cffed9efe02c575bd3b25d8ec8
linnv <win27v@gmail.com>
2014-04-15 08:19:47 +0800

    add acm demo

---
+++ acm/collection.cpp
@@ -0,0 +1,156 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class Collection{
+	private:
+		int _timeLimit;
+		int _memoryLimit;
+		int  _fileSizeLimit;
+		int _state;
+		int _judgeState;
+		int _runId;
+		int _languageId;
+		int _timeComsupted;
+		int _memoryComsupted;
+		int _userId;
+		int _problemId;
+
+		string _compilerName;
+		string _sCodeSuffix;
+		string _executeSuffix;
+		string _sCode;
+		string _stdInput;
+		string _stdOutput;
+		string _userOutput;
+		vector<string> _testCase;
+	public:
+		void setTimeLimit(const int &id){
+			_timeLimit= id;
+		}
+		int getTimeLimit(){
+			return _timeLimit;
+		}
+
+		void setMemoryLimit(const int &id){
+			_memoryLimit= id;
+		}
+		int getMemoryLimit(){
+			return _memoryLimit;
+		}
+
+		void setFileSizeLimit(const int &id){
+			_fileSizeLimit= id;
+		}
+		int getFileSizeLimit(){
+			return _fileSizeLimit;
+		}
+
+
+		void setJudgeState(const int &id){
+			_judgeState= id;
+		}
+		int getJudgeState(){
+			return _judgeState;
+		}
+
+		void setRunId(const int &id){
+			_runId= id;
+		}
+		int getRunId(){
+			return _runId;
+		}
+		void setLanguageId(const int &id){
+			_languageId= id;
+		}
+		int getLanguageId(){
+			return _languageId;
+		}
+
+		void setTimeComsupted(const int &id){
+			_timeComsupted = id;
+		}
+		int getTimeComsupted(){
+			return _timeComsupted;
+		}
+
+		void setMemoryComsupted(const int &id){
+			_memoryComsupted = id;
+		}
+		int getMemoryComsupted(){
+			return _memoryComsupted;
+		}
+
+		void setUserId(const int &id){
+			_userId = id;
+		}
+		int getUserId(){
+			return _userId;
+		}
+
+		void setExecuteSuffix(const string &id){
+			_executeSuffix = id;
+		}
+		string getExecuteSuffix(){
+			return _executeSuffix;
+		}
+
+		void setSCodeSuffix(const string &id){
+			_sCodeSuffix = id;
+		}
+		string getSCodeSuffix(){
+			return _sCodeSuffix;
+		}
+
+
+		void setCompilerName(const string &id){
+			_compilerName = id;
+		}
+		string getCompilerName(){
+			return _compilerName;
+		}
+
+
+		void setProblemId(const int &id){
+			_problemId = id;
+		}
+		int getProblemId(){
+			return _problemId;
+		}
+
+		void setState(const int&s){
+		_state = s;	
+		}
+		int getState(){
+		return _state;	
+		}
+		
+		void setSourceCode(string & str){
+		 _sCode = str;	
+		}
+		string getSourceCode(){
+			return _sCode;	
+		}
+		
+		void setSTDIput(const string &str){
+		_stdInput = str;	
+		}
+		string getSTDIput(){
+			return _stdInput;	
+		}
+
+		void setSTDOutput(string &str){
+		_stdOutput = str;	
+		}
+		string getSTDOutput(){
+			return _stdOutput;	
+		}
+
+		void setUserOutput(string &str){
+		_userOutput= str;	
+		}
+		string getUserOutput(){
+			return _userOutput;	
+		}
+
+};
--- acm/main.cpp
+++ acm/main.cpp
@@ -1,6 +1,7 @@
 #include "include.h"
 #include "mysql_connection.h"
 #include "mysqlcpp.cpp"
+#include "collection.cpp"
 using namespace std;
 int writeFromString( string &fileName, const string& buffer, size_t count);
 int readToString(string &fileName, string* str);
@@ -19,14 +20,73 @@ int main(int argc, char *argv[])
 	sqlconn->setUser("root");
 	sqlconn->setPasswd("a");
 	sqlconn->connectSQL();
-	sqlconn->useDatabase("test");
+	sqlconn->useDatabase("goj");
 	
-	sqlconn->querySQL("select * from id");
+
+//	sqlconn->querySQL("select * from id where id =2");
+	sqlconn->querySQL("SELECT * FROM tbl_run WHERE Status = 100000 AND Auto_Judge = 1 ");
+	//sqlconn->querySQL("SELECT Run_ID, Problem_ID, User_ID, Language_ID, Source_Code FROM tbl_run WHERE Status = 100000 AND Auto_Judge = 1 ");
 	sql::ResultSet *res = sqlconn->getResultSet();
 	cout<<"colunm num: "<<sqlconn->getColunmsCount()<<endl;
-	cout<<"row count:"<<sqlconn->getRowsCount()<<endl;
-		
+	//num of new collection according to the rows
+	cout<<"row count:"<<sqlconn->getRowsCount()<<endl<<endl;
+
+	int waitintCount = sqlconn->getRowsCount();
+
+	Collection *col[waitintCount];
+	for (int i = 0; i < waitintCount; ++i)
+	{
+		col[i] = new Collection();
+
+	}
+	int count = waitintCount;
+	string tmp;
+	int tmpInt;
+	while (count){				
+			{
+
+				res->next();
+				tmpInt = res->getInt("Run_ID");
+				col[waitintCount-count]->setRunId(tmpInt);
+
+				/*
+				tmp = res->getString("Source_Code");
+				col[waitintCount-count]->setSourceCode(tmp);
+
+				tmp = res->getString("Source_Code");
+				col[waitintCount-count]->setSourceCode(tmp);
+				*/
+				tmp = res->getString("Source_Code");
+				col[waitintCount-count]->setSourceCode(tmp);
+
+
+			//cout<<res->getString("name")<<" ";
+			//	str+=res->getString("name");
+				//cout<<res->getString(i)<<" ";
+				count--;
+			}
+	}
+	for (int i = 0; i < waitintCount; ++i)
+	{
+		cout<<i<<"run id:"<<col[i]->getRunId()<<" source code: \n"<<col[i]->getSourceCode()<<endl;
+
+	}
+
 	/*
+	string tmp;
+	while (res->next()){				
+		for (int i = 1; i <= sqlconn->getRowsCount(); ++i)
+			{
+				tmp = res->getString("name");
+				col->setSourceCode(tmp);
+				//cout<<res->getString("name")<<" ";
+			//	str+=res->getString("name");
+				//cout<<res->getString(i)<<" ";
+			}
+		cout<<endl;
+	}
+	cout<<col->getSourceCode()<<endl;
+	
 	//insert
 	sqlconn->updateSQL("insert into id set id = 27,name = 'jialin'");
 	//update
