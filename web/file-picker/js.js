59cd70c494e7c0af902ab5d7a4c5895f306ff6e4
JialinWu <win27v@gmail.com>
2016-04-25 14:44:46 +0800

    diretory reconize, support go to upper dir

--- web/file-picker/js.js
+++ web/file-picker/js.js
@@ -3,26 +3,47 @@ function getData(){
 }
 
 var xmlr
-var ga
 var currentPath
+
+function prePath(){
+	xmlr=new XMLHttpRequest();
+	xmlr.onreadystatechange=callback;
+	var url;
+	if (currentPath!=undefined) {
+		if (currentPath.slice(-1)=='/') {
+			var p1=currentPath.substr(0,currentPath.lastIndexOf("/"));
+			var p2=p1.substr(0,p1.lastIndexOf("/"));
+			url="http://127.0.0.1:8099/file-staus"+"?path="+p2;
+		}else{
+			var p1=currentPath.substr(0,currentPath.lastIndexOf("/"));
+			url="http://127.0.0.1:8099/file-staus"+"?path="+p1;
+		}
+	}else{
+		url="http://127.0.0.1:8099/file-staus";
+	}
+	xmlr.open("GET",url,true);
+	xmlr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+	xmlr.send(null);
+}
+
 function ajaxClear(){
-			document.getElementById("get-response").innerHTML="";
-			currentPath=undefined;
+	currentPath=undefined;
+	ajaxGet();
 }
 
 function ajaxGet(attributes){
-	ga =attributes
 	xmlr=new XMLHttpRequest();
 	xmlr.onreadystatechange=callback;
-	// xmlr.open("GET","http://127.0.0.1:8099/get",false);
-	// async
+	var url;
 	if (currentPath!=undefined) {
-	var url="http://127.0.0.1:8099/file-staus"+"?path="+currentPath+"/"+ga.innerHTML;
-	// var url="http://127.0.0.1:8099/file-staus";
+		if (currentPath.slice(-1)=='/') {
+			url="http://127.0.0.1:8099/file-staus"+"?path="+currentPath+attributes.innerHTML;
+		}else{
+			url="http://127.0.0.1:8099/file-staus"+"?path="+currentPath+"/"+attributes.innerHTML;
+		}
 	}else{
-	var url="http://127.0.0.1:8099/file-staus";
+		url="http://127.0.0.1:8099/file-staus";
 	}
-	console.log(url);
 	xmlr.open("GET",url,true);
 	xmlr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
 	xmlr.send(null);
@@ -31,29 +52,22 @@ function ajaxGet(attributes){
 function callback(){
 	if (xmlr.readyState==4) {
 		if (xmlr.status==200) {
-			// console.log(ga.innerHTML);
 			var obj = JSON.parse(xmlr.responseText);
 			currentPath=obj.AbsPath;
-			var tablestr="<tr><td>name</td><td>Mb</td><td>Bytes</td></tr>";
-			// tablestr+="<tr><td>"+String(obj.AbsPath)+"</td><td>"+String((parseInt(obj.TotalSize)/1024/1024).toFixed(2))+"</td><td>"+String(obj.TotalSize)+"</td></tr>";	
+			var tablestr="<tr><td><a onclick=prePath()>..</a></td></tr>";
+			tablestr+="<tr><td>name</td><td>Mb</td><td>Bytes</td></tr>";
 			tablestr+="<tr><td>"+String(obj.AbsPath)+"</td><td>"+String((parseInt(obj.TotalSize)/1024/1024).toFixed(2))+"</td><td>"+String(obj.TotalSize)+"</td></tr>";	
 			for (var i = 0; i <= obj.Items.length - 1; i++) {
 				tablestr+="<tr>";
-				tablestr=tablestr+"<td>"+"<a onclick=ajaxGet(this)>"+String(obj.Items[i].Name)+"</a>"+"</td>"+"<td>"+String((parseInt(obj.Items[i].Size)/1024/1024).toFixed(2))+"</td>"+"<td>"+String(obj.Items[i].Size)+"</td>";
-				// tablestr=tablestr+"<td>"+String(obj.Items[i].Name)+"</td>"+"<td>"+String((parseInt(obj.Items[i].Size)/1024/1024).toFixed(2))+"</td>"+"<td>"+String(obj.Items[i].Size)+"</td>";
+				if (obj.Items[i].Name.slice(-1)=='/') {
+					tablestr=tablestr+"<td>"+"<a onclick=ajaxGet(this)>"+String(obj.Items[i].Name)+"</a>"+"</td>"+"<td>"+String((parseInt(obj.Items[i].Size)/1024/1024).toFixed(2))+"</td>"+"<td>"+String(obj.Items[i].Size)+"</td>";
+				}else{
+					tablestr=tablestr+"<td>"+String(obj.Items[i].Name)+"</td>"+"<td>"+String((parseInt(obj.Items[i].Size)/1024/1024).toFixed(2))+"</td>"+"<td>"+String(obj.Items[i].Size)+"</td>";
+				}
 				tablestr+="</tr>";
-			
-			// }
-			// for (var i in obj.Items) {
-				// console.log(obj[i]);
-				// tablestr+="<tr>";
-				// tablestr=tablestr+"<td>"+String(obj[i].Name)+"</td>"+"<td>"+String(obj[i].Size)+"</td>";
-				// tablestr+="</tr>";
 			}
 
 			document.getElementById("get-response").innerHTML=tablestr;
 		}
 	}
 }
-
-
