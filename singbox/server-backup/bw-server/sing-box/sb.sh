0be161bcaf3f1611f3c846f2c599b4d03f02c80b
Jialin Wu <win27v@gmail.com>
2024-08-06 09:35:36 +0800

    sing-box: *

---
+++ singbox/server-backup/bw-server/sing-box/sb.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/fscarmen/sing-box/main/sing-box.sh) $1
---
+++ singbox/testws.sh
@@ -0,0 +1,10 @@
+curl --include \
+     --no-buffer \
+     --header "Connection: Upgrade" \
+     --header "Upgrade: websocket" \
+     --header "Host: singbox.jialinwu.com:2083" \
+     --header "Origin: https://singbox.jialinwu.com:2083" \
+     --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" \
+     --header "Sec-WebSocket-Version: 13" \
+     https://singbox.jialinwu.com:2083/singbox
+
